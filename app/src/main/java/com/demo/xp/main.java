8d088f832f3b50cc434f212b5088628f8be14c7f
m369848622 <369848622@qq.com>
2022-08-09 18:19:32 +0800

    111

--- app/src/main/java/com/demo/xp/main.java
+++ app/src/main/java/com/demo/xp/main.java
@@ -1,6 +1,9 @@
 package com.demo.xp;
 
 
+import android.app.Application;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
@@ -13,20 +16,43 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class main implements IXposedHookLoadPackage {
+    public Context Main_context;
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpp) throws Throwable {
         String packageName = lpp.packageName;
         ClassLoader classLoader = lpp.classLoader;
+        if (Main_context == null) {
+            XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    Main_context = (Context) param.args[0];
+                }
+            });
+        }
 
         XposedBridge.log("start->" + packageName);
-        XposedHelpers.findAndHookConstructor("android.content.Intent",classLoader,String.class, Uri.class, new XC_MethodHook() {
+        XposedHelpers.findAndHookConstructor("android.content.Intent", classLoader, String.class, Uri.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                param.setResult(null);
+                Intent intent = start(Main_context);
+                param.setResult(intent);
             }
         });
 
     }
 
-   }
+    Intent start(Context context) {
+        Intent intent = new Intent();
+        ComponentName comp = new ComponentName("com.atp.voiceapp", "com.tencent.liteav.demo.MainActivity");
+        intent.setComponent(comp);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction("android.intent.action.SEND");
+        intent.setType("text/plain");
+        XposedBridge.log("call->" + "18615596666");
+        intent.putExtra(Intent.EXTRA_TEXT, "16464");
+        context.startActivity(intent);
+        return intent;
+    }
+}
