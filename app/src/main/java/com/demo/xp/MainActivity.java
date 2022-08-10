59c7a4a9eb8a9b491a1bcd5d9585221722c35a84
m369848622 <369848622@qq.com>
2022-08-10 21:25:30 +0800

    333

--- app/src/main/java/com/demo/xp/MainActivity.java
+++ app/src/main/java/com/demo/xp/MainActivity.java
@@ -1,19 +1,93 @@
 package com.demo.xp;
 
+import static com.demo.xp.common.readFileString;
+import static com.demo.xp.common.writeFileString;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.logging.Logger;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements VolumeChangeObserver.VolumeChangeListener {
 
+    private VolumeChangeObserver mVolumeChangeObserver;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            Log.i("sdfljksdlfk", "DOWN");
+        }
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            Log.i("sdfljksdlfk", "UP");
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    @Override
+    protected void onResume() {
+        //注册广播接收器
+        mVolumeChangeObserver.registerReceiver();
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        //解注册广播接收器
+        mVolumeChangeObserver.unregisterReceiver();
+        super.onPause();
+    }
+    @Override
+
+    public void onVolumeChanged(int volume) {
+        //系统媒体音量改变时的回调
+        Log.e("sdfsdf", "onVolumeChanged()--->volume = " + volume);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        mVolumeChangeObserver = new VolumeChangeObserver(this);
+        mVolumeChangeObserver.setVolumeChangeListener(this);
+//        int initVolume = mVolumeChangeObserver.getCurrentMusicVolume();
+        if (Build.VERSION.SDK_INT >= 23 && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
         setContentView(R.layout.activity_main);
+        Button bt = (Button) findViewById(R.id.button);
+        String sdcardPath = Environment.getExternalStorageDirectory().toString();
+        File file = new File(sdcardPath + "/phone.txt");
+        if (!file.exists()) {// 判断文件目录是否存在
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            bt.setText(readFileString(file));
+        }
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = ((TextView) findViewById(R.id.phone)).getText().toString();
+                writeFileString(file, phone);
+                bt.setText(phone);
+            }
+        });
     }
 
 
--- app/src/main/java/com/demo/xp/main.java
+++ app/src/main/java/com/demo/xp/main.java
@@ -1,12 +1,20 @@
 package com.demo.xp;
 
 
+import static com.demo.xp.common.readFileString;
+
 import android.app.Application;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
+import java.io.File;
+import java.lang.reflect.Field;
 import java.net.URI;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -14,6 +22,7 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import external.org.apache.commons.lang3.StringUtils;
 
 public class main implements IXposedHookLoadPackage {
     public Context Main_context;
@@ -32,18 +41,51 @@ public class main implements IXposedHookLoadPackage {
             });
         }
 
+
         XposedBridge.log("start->" + packageName);
         XposedHelpers.findAndHookConstructor("android.content.Intent", classLoader, String.class, Uri.class, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                Intent intent = start(Main_context, String.valueOf(param.args[1]));
-                param.setResult(intent);
+                String sdcardPath = Environment.getExternalStorageDirectory().toString();
+                File file = new File(sdcardPath + "/phone.txt");
+                String aphone = readFileString(file);
+                if (aphone != null && aphone.length() > 0) {
+                    Log.i("sldfdsf", "aphone1");
+                    Intent intent = start(Main_context, String.valueOf(param.args[1]), aphone);
+                    param.setResult(intent);
+                } else {
+                    Log.i("sldfdsf", "aphone2");
+                    super.beforeHookedMethod(param);
+                }
             }
         });
 
+        XposedHelpers.findAndHookMethod(Toast.class, "show", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                param.setResult(null);
+            }
+        });
+
+//        XposedHelpers.findAndHookConstructor("com.android.dialer.main.impl.MainActivity.onKeyDown", classLoader, new XC_MethodHook() {
+//            @Override
+//            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                int keyCode = Integer.parseInt(String.valueOf(param.args[0]));
+//                if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+//                    Log.i("sdfljksdlfk", "DOWN");
+//                }
+//                if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+//                    Log.i("sdfljksdlfk", "UP");
+//                }
+//            }
+//
+//        });
+
+
     }
 
-    Intent start(Context context,String phone) {
+    Intent start(Context context, String phone, String aphone) {
+        XposedBridge.log("start->" + aphone);
         Intent intent = new Intent();
         ComponentName comp = new ComponentName("com.atp.voiceapp", "com.tencent.liteav.demo.MainActivity");
         intent.setComponent(comp);
@@ -51,7 +93,7 @@ public class main implements IXposedHookLoadPackage {
         intent.setAction("android.intent.action.SEND");
         intent.setType("text/plain");
         XposedBridge.log("call->" + phone.split("tel:")[1]);
-        intent.putExtra(Intent.EXTRA_TEXT, "666,"+phone.split("tel:")[1].replace("%20",""));
+        intent.putExtra(Intent.EXTRA_TEXT, aphone + "," + phone.split("tel:")[1].replace("%20", ""));
         context.startActivity(intent);
         return intent;
     }
